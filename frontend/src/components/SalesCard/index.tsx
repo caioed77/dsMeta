feb2d5f231ef47477edd6a431a36920c1c4c8de6
CAIO <caioeduardosantos7@gmail.com>
2022-07-14 21:03:51 -0300

    create UseState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,16 @@ import NotificationButton from '../NotificationButton'
 import './styles.css'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
 function SalesCard() {
+   
+    const max = new Date();
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+
+    const [minDate, setMinDate] = useState(max);
+    const [maxDate, setMaxDate] = useState(min);
+   
+   
     return (
         <>
             <div className="dsmeta-container">
@@ -11,16 +20,16 @@ function SalesCard() {
                     <div>
                         <div className="dsmeta-form-control-container">
                             <DatePicker
-                                selected={new Date()}
-                                onChange={(date: Date) => { }}
+                                selected={maxDate}
+                                onChange={(date: Date) => setMaxDate(date)}
                                 className="dsmeta-form-control"
                                 dateFormat="dd/MM/yyyy"
                             />
                         </div>
                         <div className="dsmeta-form-control-container">
                             <DatePicker
-                                selected={new Date()}
-                                onChange={(date: Date) => { }}
+                                selected={minDate}
+                                onChange={(date: Date) => setMinDate(date)}
                                 className="dsmeta-form-control"
                                 dateFormat="dd/MM/yyyy"
                             />
@@ -98,3 +107,7 @@ function SalesCard() {
 }
 
 export default SalesCard
+
+function usestate(): [any, any] {
+    throw new Error('Function not implemented.');
+}
